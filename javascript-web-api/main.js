e982aa43b8f93398566f50722d4d7790cf55d02c
AdemCan <69676558+AdemCan0BEY@users.noreply.github.com>
2020-11-05 20:14:46 +0300

    Update main.js

--- javascript-web-api/main.js
+++ javascript-web-api/main.js
@@ -1,13 +1,13 @@
-var express = require("express") // kurduğumuz express modülünü dahil ediyoruz
+var express = require("express") // we include the express module we installed
 var app = express()
-app.get('/', function (req, resp) { // get methodumuzu ekliyoruz
-    var response = { message:"Merhaba Dünya" } //geriye döndürülecek modelimizi oluşturuyoruz
-    resp.json(response); //json formatında döndürüyoruz
+app.get('/', function (req, resp) { // We add our get method
+    var response = { message:"Hello, world" } //We create our model to be reverted
+    resp.json(response); //We return in json format
 })
-app.get('/hakkimda', function (req, resp) { 
-    var response = { message:"Hakkimda" } 
+app.get('/about', function (req, resp) { 
+    var response = { message:"About" } 
     resp.json(response); 
 })
-//aşağıdaki kod ile  "http://localhost:1881" üzerinden web apimizi çalıştırıyoruz
-//Dipnot: 1881 Atatürk'ün Doğum Tarihi 
-app.listen("1881", () => console.log("localhost çalıştırıldı Port: 1881"))
+//with the code below  "http://localhost:1881" We run our web ap via
+//Dipnot: 1881 Atatürk'ün Date of birth 
+app.listen("1881", () => console.log("localhost executed Port: 1881"))
